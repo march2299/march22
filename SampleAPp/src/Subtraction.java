526d33d3dacbd5b73e2628c983a3652a8a1e71e9
march2299 <javabatchmar22@gmail.com>
2022-09-28 15:15:17 +0530

    adding subtraction program by manasi

---
+++ SampleAPp/src/Subtraction.java
@@ -0,0 +1,12 @@
+public class Subtraction {
+    public static void main(String args[]) {
+        int a = 20;
+        int b = 10;
+        int c = a - b;
+
+        System.out.println("The Subtraction is " + c);
+    }
+}
+
+
+
